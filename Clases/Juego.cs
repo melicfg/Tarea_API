6fdd840dee378b01010a069c7f9024edf9205c71
Melissa Cardenas <40185462+melicfg@users.noreply.github.com>
2023-12-07 18:58:15 -0600

    Classes added

---
+++ Clases/Juego.cs
@@ -0,0 +1,14 @@
+﻿namespace Tarea.Clases
+{
+    public class Juego
+    {
+
+        public Juego() { }
+
+        public int Id_Juego { get; set; }
+        public int Serial_Number { get; set; }
+        public int Anio_Publicacion { get; set; }
+        public string Casa_Fabricante { get; set; }
+        public List<TipoDeJuego> Tipo_De_Juego { get; set; }
+    }
+}
---
+++ Clases/TipoDeJuego.cs
@@ -0,0 +1,12 @@
+﻿namespace Tarea.Clases
+{
+    public class TipoDeJuego
+    {
+
+        public TipoDeJuego() { }
+
+        public int idTipo { get; set; }
+        public string descripcion { get; set; }
+
+    }
+}
